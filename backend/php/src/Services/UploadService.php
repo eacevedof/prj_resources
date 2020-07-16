7798a73be84a08c3cce547daf140c7c0606e9f9e
ioedumac <eacevedof@yahoo.es>
2020-07-16 16:42:17 +0200

    validando extensiones

--- backend/php/src/Services/UploadService.php
+++ backend/php/src/Services/UploadService.php
@@ -8,6 +8,9 @@ class UploadService extends AppService
     private $post;
     private $rootpath;
     private $url;
+    const INVALID_EXTENSIONS = [
+        "php","js","py",
+    ];
 
     public function __construct($post,$files)
     {
@@ -20,30 +23,40 @@ class UploadService extends AppService
     {
         if(!$this->post) throw new Exception("Empty post");
         if(!$this->files) throw new Exception("Empty files");
-        if(!isset($this->post["folderdomain"])) throw new Exception("No domain selected");
+        if(!isset($this->post["folderdomain"]) || trim($this->post["folderdomain"])==="") throw new Exception("No domain selected");
     }
 
-    private function _is_postok()
-    {
+    private function _get_basename($rawname){return basename($rawname);}
 
-    }
-
-    private function _get_basename($rawname)
-    {
-        return basename($rawname);
-    }
+    private function _get_extension($pathfile){return pathinfo($pathfile, PATHINFO_EXTENSION);}
 
     private function _get_saved($pathfinal)
     {
         return move_uploaded_file($this->files["fil-one"]["tmp_name"],$pathfinal);
     }
+
+    private function _is_invalid($extension)
+    {
+        $extension = trim($extension);
+        $extension = strtolower($extension);
+        return in_array($extension,self::INVALID_EXTENSIONS);
+    }
+
     
     private function _upload()
     {
+        $extension = $this->_get_extension($this->files["fil-one"]["name"]);
+        if($this->_is_invalid($extension)){
+            $this->add_error("file: {$this->files["fil-one"]["name"]} contains forbidden extension");
+            return;
+        }
+
         $today = date("Ymd");
-        $pathdest = "{$this->rootpath}/{$today}/";
+        $folderdomain = $this->post["folderdomain"];
+        $pathdest = "{$this->rootpath}/$folderdomain/{$today}";
         if(!file_exists($pathdest)) mkdir($pathdest, 0777, true);
         $filename = $this->_get_basename($this->files["fil-one"]["name"]);
+
         $pathfinal = "{$pathdest}/{$filename}";
         if(is_file($pathfinal)) unlink($pathfinal);
         //print_r($pathfinal);die;
@@ -59,4 +72,15 @@ class UploadService extends AppService
         return $this->url;
     }
 
+
 }
+/*
+'fil-one' =>
+array (
+'name' => 'trello-397002984.jpg',
+'type' => 'image/jpeg',
+'tmp_name' => '/private/var/folders/yt/g9dtf4cj40s6m5b4m_rzjz8m0000gn/T/phpKQHL21',
+'error' => 0,
+'size' => 504284,
+),*
+*/
