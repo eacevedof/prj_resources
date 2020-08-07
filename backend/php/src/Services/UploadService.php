afe9922c40487f86cc819d21afb5299a54c2c4b1
ioedumac <eacevedof@yahoo.es>
2020-08-07 18:43:47 +0200

    fix de memory_limit=-1

--- backend/php/src/Services/UploadService.php
+++ backend/php/src/Services/UploadService.php
@@ -34,7 +34,7 @@ class UploadService extends AppService
         //$memory_limit = (int)(ini_get('memory_limit'));//en prod me devuelve -1
         $upload_mb = min($max_upload, $max_post);
         //en prod son 64M para upload y post
-        lg("get_maxsize(): upload_max_filesize:$max_upload, post_max_size:$max_post","get_maxsize");
+        //lg("get_maxsize(): upload_max_filesize:$max_upload, post_max_size:$max_post","get_maxsize");
         return $upload_mb;
     }
     
